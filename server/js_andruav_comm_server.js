7fe53106f59c1d98286c66444bfa0c5eebae77d2
HefnySco <mohammad.hefny@gmail.com>
2025-03-09 03:50:00 +0300

    formating doc

--- server/js_andruav_comm_server.js
+++ server/js_andruav_comm_server.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const m_commServerManagerClient = require ("./js_comm_server_manager_client");
-var m_agent_chat_server; 
+const m_commServerManagerClient = require("./js_comm_server_manager_client");
+var m_agent_chat_server;
 const c_ChatAccountRooms = require("./js_andruav_chat_account_rooms");
 const { v4: uuidv4 } = require('uuid');
-const c_CONSTANTS = require ("../js_constants");
+const c_CONSTANTS = require("../js_constants");
 const v_version = require('../package.json').version;
 
 /**
@@ -12,17 +12,14 @@ const v_version = require('../package.json').version;
  * TODO: NOT IMPLEMENTED
  * @param {raw message from Auth} p_msg 
  */
-function fn_decryptAuthMessage (p_msg)
-{
-    try
-    {
-        const v_msg =  JSON.parse(p_msg);
+function fn_decryptAuthMessage(p_msg) {
+    try {
+        const v_msg = JSON.parse(p_msg);
 
         return v_msg;
     }
-    catch (ex)
-    {
-        console.log (ex);
+    catch (ex) {
+        console.log(ex);
     }
 
     return null;
@@ -33,19 +30,18 @@ function fn_decryptAuthMessage (p_msg)
  * @param {*} p_cmd request from AUTH_SERVER
  * @returns 
  */
-function fn_generateLoginRequestReply (p_cmd)
-{
+function fn_generateLoginRequestReply(p_cmd) {
     const c_reply = {
-        'c':c_CONSTANTS.CONST_CS_CMD_LOGIN_REQUEST, // info
+        'c': c_CONSTANTS.CONST_CS_CMD_LOGIN_REQUEST, // info
         'd': {}
     };
 
-    c_reply.d [c_CONSTANTS.CONST_CS_REQUEST_ID.toString()] = p_cmd.d[c_CONSTANTS.CONST_CS_REQUEST_ID.toString()];
-    c_reply.d [c_CONSTANTS.CONST_CS_ERROR.toString()] = c_CONSTANTS.CONST_ERROR_NON;
-    c_reply.d [c_CONSTANTS.CONST_CS_SERVER_PUBLIC_HOST.toString()] = global.m_serverconfig.m_configuration.public_host;
-    c_reply.d [c_CONSTANTS.CONST_CS_SERVER_PORT.toString()] = global.m_serverconfig.m_configuration.server_port;
-    c_reply.d [c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()] = p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()];
-    
+    c_reply.d[c_CONSTANTS.CONST_CS_REQUEST_ID.toString()] = p_cmd.d[c_CONSTANTS.CONST_CS_REQUEST_ID.toString()];
+    c_reply.d[c_CONSTANTS.CONST_CS_ERROR.toString()] = c_CONSTANTS.CONST_ERROR_NON;
+    c_reply.d[c_CONSTANTS.CONST_CS_SERVER_PUBLIC_HOST.toString()] = global.m_serverconfig.m_configuration.public_host;
+    c_reply.d[c_CONSTANTS.CONST_CS_SERVER_PORT.toString()] = global.m_serverconfig.m_configuration.server_port;
+    c_reply.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()] = p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()];
+
     return c_reply;
 }
 
@@ -54,8 +50,7 @@ function fn_generateLoginRequestReply (p_cmd)
  * Called whenever a connection established with AuthServer.
  * There is always one single Auth server.
  */
-function fn_AuthServerConnectionHandler ()
-{
+function fn_AuthServerConnectionHandler() {
     fn_updateServerWatchdog();
 }
 
@@ -67,16 +62,15 @@ function fn_AuthServerConnectionHandler ()
  * p_cmd.c = 'b' for new login request.
  * p_cmd.d{a: SID, at: actor type (d for drone) d, b:GroupID=1, r: requestID GUID, f: login_temp_key (instead of party ID)}
  */
- function fn_handleLoginResponses (p_cmd)
-{
+function fn_handleLoginResponses(p_cmd) {
     // Generates CONST_CS_LOGIN_TEMP_KEY to be sent to agent through AndruavAuth
     // Agent will use this key to connect to AndruavServer.
-    p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()] = uuidv4().replaceAll('-','');
+    p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()] = uuidv4().replaceAll('-', '');
     // add it to waiting list so that can be either deleted when timeout or retrieved when agent connects to AndruavServer.            
-    m_agent_chat_server.fn_addWaitingAccount (p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()],p_cmd.d);
-    console.log ("New Party: " + JSON.stringify(p_cmd));
+    m_agent_chat_server.fn_addWaitingAccount(p_cmd.d[c_CONSTANTS.CONST_CS_LOGIN_TEMP_KEY.toString()], p_cmd.d);
+    console.log("New Party: " + JSON.stringify(p_cmd));
 
-    const c_reply = fn_generateLoginRequestReply (p_cmd);
+    const c_reply = fn_generateLoginRequestReply(p_cmd);
 
     m_commServerManagerClient.fn_sendMessage(JSON.stringify(c_reply));
 }
@@ -85,64 +79,57 @@ function fn_AuthServerConnectionHandler ()
  * Handles messages received from Auth Server. It is called per message.
  * @param {raw messages from auth server} p_msg 
  */
-function fn_AuthServerMessagesHandler (p_msg)
-{
-    try
-    {
-        const p_cmd = fn_decryptAuthMessage (p_msg);
-        if ((p_cmd == null) || (!p_cmd.hasOwnProperty('c')))
-        {
+function fn_AuthServerMessagesHandler(p_msg) {
+    try {
+        const p_cmd = fn_decryptAuthMessage(p_msg);
+        if ((p_cmd == null) || (!p_cmd.hasOwnProperty('c'))) {
             // invalid command
             return;
         }
 
-        switch (p_cmd.c)
-        {
+        switch (p_cmd.c) {
             case c_CONSTANTS.CONST_CS_CMD_LOGIN_REQUEST:
-            {
-                //return ; // testing to generate timeout at authenticator.
-                
-                if ((!p_cmd.hasOwnProperty('d'))
-                    || p_cmd.d.hasOwnProperty('d')
-                    )
-                    break;
-                fn_handleLoginResponses(p_cmd);
-            }
-            break;
+                {
+                    //return ; // testing to generate timeout at authenticator.
 
-            
+                    if ((!p_cmd.hasOwnProperty('d'))
+                        || p_cmd.d.hasOwnProperty('d')
+                    )
+                        break;
+                    fn_handleLoginResponses(p_cmd);
+                }
+                break;
+
+
             case c_CONSTANTS.CONST_CS_CMD_LOGOUT_REQUEST:
-            {
-                //@todo: not implemented    
-                
-            }
-            break;
+                {
+                    //@todo: not implemented    
+
+                }
+                break;
         }
     }
-    catch (ex)
-    {
-        console.log ("err: fn_AuthServerMessagesHandler: " + ex);
+    catch (ex) {
+        console.log("err: fn_AuthServerMessagesHandler: " + ex);
     }
 }
 
 
-function fn_updateServerWatchdog()
-{
-    try
-    {
+function fn_updateServerWatchdog() {
+    try {
         // prepare my Info Card
-        var v_obj = { 
-                    'isOnline':true,
-                    'version': v_version,
-                    'serverId':global.m_serverconfig.m_configuration.server_id,
-                    'public_host':global.m_serverconfig.m_configuration.public_host, // this is the ip that is listening to the connections.
-                    'serverPort':global.m_serverconfig.m_configuration.server_port,
-                    // this is why we send this message with every connection.
-                    // it is crucial to Auth server to know the exact unique list of keys
-                    // so that it can rout correctly in case of multiple communication_servers
-                    'accounts':c_ChatAccountRooms.fn_getUnitKeys() 
-                };
-        
+        var v_obj = {
+            'isOnline': true,
+            'version': v_version,
+            'serverId': global.m_serverconfig.m_configuration.server_id,
+            'public_host': global.m_serverconfig.m_configuration.public_host, // this is the ip that is listening to the connections.
+            'serverPort': global.m_serverconfig.m_configuration.server_port,
+            // this is why we send this message with every connection.
+            // it is crucial to Auth server to know the exact unique list of keys
+            // so that it can rout correctly in case of multiple communication_servers
+            'accounts': c_ChatAccountRooms.fn_getUnitKeys()
+        };
+
         // send Info Card to Andruav Auth
         m_commServerManagerClient.fn_sendMessage(JSON.stringify(
             {
@@ -150,55 +137,51 @@ function fn_updateServerWatchdog()
                 'd': v_obj  // data
             }));
     }
-    catch (ex)
-    {
-        console.log ("Error:fn_updateServerWatchdog:" + ex.toString());
+    catch (ex) {
+        console.log("Error:fn_updateServerWatchdog:" + ex.toString());
     }
 }
 
 /**
  * Start Server
  */
-function fn_startServer ()
-{
+function fn_startServer() {
 
-    console.log (global.Colors.Success + "[OK] Communication Server Started"  + global.Colors.Reset);
+    console.log(global.Colors.Success + "[OK] Communication Server Started" + global.Colors.Reset);
 
-    if (global.m_serverconfig.m_configuration.allow_fake_SSL ===true)
-    {
+    if (global.m_serverconfig.m_configuration.allow_fake_SSL === true) {
         // ATTENTION: should be FALSE in PRODUCTION
         // skip verifying fake SSL
-        console.log (global.Colors.BFgYellow + "ATTENTION!!  SECURITY RISK . NODE_TLS_REJECT_UNAUTHORIZED = 0" + global.Colors.Reset);
-        console.log (global.Colors.BFgYellow + "THIS IS ACCEPTED ONLY IF COMM with ANDRUAV_AUTH is 127.0.0.1" + global.Colors.Reset);
+        console.log(global.Colors.BFgYellow + "ATTENTION!!  SECURITY RISK . NODE_TLS_REJECT_UNAUTHORIZED = 0" + global.Colors.Reset);
+        console.log(global.Colors.BFgYellow + "THIS IS ACCEPTED ONLY IF COMM with ANDRUAV_AUTH is 127.0.0.1" + global.Colors.Reset);
 
         process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-    } 
-    else if (global.m_serverconfig.m_configuration.ca_cert_path ===true)
-    {
+    }
+    else if (global.m_serverconfig.m_configuration.ca_cert_path === true) {
         process.env["NODE_EXTRA_CA_CERTS"] = global.m_serverconfig.m_configuration.ca_cert_path;
     }
 
-    
+
 
     // if (global.m_serverconfig.m_configuration.dynamicKEY)
     // {
     //     // initialize key once
     //     fn_refreshDynamicKeys(); 
-        
+
     //     // start a timer
     //     setInterval(function()
     //     {
     //         fn_refreshDynamicKeys()
     //     }, global.m_serverconfig.m_configuration.dynamicKEYInterval); // Time in milliseconds;
-        
+
     // }
 
-    m_commServerManagerClient.fn_onMessageReceived  = fn_AuthServerMessagesHandler;
-    m_commServerManagerClient.fn_onMessageOpened    = fn_AuthServerConnectionHandler;
+    m_commServerManagerClient.fn_onMessageReceived = fn_AuthServerMessagesHandler;
+    m_commServerManagerClient.fn_onMessageOpened = fn_AuthServerConnectionHandler;
     m_commServerManagerClient.fn_startServer();
     m_agent_chat_server = global.m_chat_server_singelton_get_instance();
     m_agent_chat_server.fn_startServer();
-    
+
 }
 
 
--- server/js_comm_server_manager_client.js
+++ server/js_comm_server_manager_client.js
@@ -9,15 +9,13 @@
 
 const CONST_S2S_WS_RETRY_TIME = 2000;
 var m_ws;
-var  Me;
+var Me;
 
-function fn_onOpen_Handler ()
-{
-    
-    console.log (global.Colors.BSuccess + "[OK] Connection established with AuthServer" + global.Colors.Reset);
+function fn_onOpen_Handler() {
 
-    if (Me.fn_onMessageOpened != null )
-    {
+    console.log(global.Colors.BSuccess + "[OK] Connection established with AuthServer" + global.Colors.Reset);
+
+    if (Me.fn_onMessageOpened != null) {
         Me.fn_onMessageOpened();
     }
 }
@@ -27,19 +25,17 @@ function fn_onOpen_Handler ()
  * Called when websocket is closed. Websocket is NEVER closed on purpose which means this is a disconnect.
  * a retry function is called with rate CONST_S2S_WS_RETRY_TIME.
  */
-function fn_onClose_Handler ()
-{
-    console.log (global.Colors.Error + "ATTENTION!! Connection closed with AuthServer" + global.Colors.Reset);
+function fn_onClose_Handler() {
+    console.log(global.Colors.Error + "ATTENTION!! Connection closed with AuthServer" + global.Colors.Reset);
 
-    const c_url= "wss://" + global.m_serverconfig.m_configuration.s2s_ws_target_ip + ":" + global.m_serverconfig.m_configuration.s2s_ws_target_port;
+    const c_url = "wss://" + global.m_serverconfig.m_configuration.s2s_ws_target_ip + ":" + global.m_serverconfig.m_configuration.s2s_ws_target_port;
 
-    setTimeout (function () {fn_startWebSocketListener(c_url)},CONST_S2S_WS_RETRY_TIME);
+    setTimeout(function () { fn_startWebSocketListener(c_url) }, CONST_S2S_WS_RETRY_TIME);
 }
 
 
-function fn_onError_Handler (err)
-{
-    console.log (global.Colors.Error + "ATTENTION!! Connection error with AuthServer " + global.Colors.Reset + err);
+function fn_onError_Handler(err) {
+    console.log(global.Colors.Error + "ATTENTION!! Connection error with AuthServer " + global.Colors.Reset + err);
 
 }
 
@@ -47,19 +43,16 @@ function fn_onError_Handler (err)
  * On receive message from AndruavAuth
  * @param {*} data 
  */
-function fn_onMessage_Handler (data)
-{
+function fn_onMessage_Handler(data) {
     //console.log ("debug ... fn_onMessage_Handler:" + data);
-    if (Me.fn_onMessageReceived != null)
-    {
-        Me.fn_onMessageReceived (data);
+    if (Me.fn_onMessageReceived != null) {
+        Me.fn_onMessageReceived(data);
     }
 }
 
 
 
-function fn_startWebSocketListener (p_url)
-{
+function fn_startWebSocketListener(p_url) {
     const WebSocket = require('ws');
     m_ws = new WebSocket(p_url);
     m_ws.on('open', fn_onOpen_Handler);
@@ -72,22 +65,19 @@ function fn_startWebSocketListener (p_url)
 /**
  * start communication server manager client module.
  */
-function fn_startServer ()
-{
+function fn_startServer() {
     Me = this;
-    const c_url= "wss://" + global.m_serverconfig.m_configuration.s2s_ws_target_ip + ":" + global.m_serverconfig.m_configuration.s2s_ws_target_port;
+    const c_url = "wss://" + global.m_serverconfig.m_configuration.s2s_ws_target_ip + ":" + global.m_serverconfig.m_configuration.s2s_ws_target_port;
 
-    console.log (global.Colors.Success + "[OK] Comm Server Manager Client connecting to " + c_url + " to reach AndruavAuth" + global.Colors.Reset);
+    console.log(global.Colors.Success + "[OK] Comm Server Manager Client connecting to " + c_url + " to reach AndruavAuth" + global.Colors.Reset);
 
     fn_startWebSocketListener(c_url);
 }
 
 
-function fn_sendMessage (p_message)
-{
-    if (m_ws != null)
-    {
-        m_ws.send (p_message);
+function fn_sendMessage(p_message) {
+    if (m_ws != null) {
+        m_ws.send(p_message);
     }
 }
 
